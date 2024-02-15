bf98366bb8dc9c9b7170f576df053fe1d4050b0c
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-02-15 15:10:20 -0300

    Criação do componente UserMenu

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -24,6 +24,7 @@ const Navbar = () => {
                 >
                     <Logo />
                     <Search />
+                    <UserMenu />
                 </div>
             </Container>
         </div>
