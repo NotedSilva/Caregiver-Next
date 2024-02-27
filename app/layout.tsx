8afd81c0594c957a704061e6cdd3336b18c47f28
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-02-27 15:47:24 -0300

    Melhoria na estrutura do layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,46 @@
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "./globals.css";
+import { Nunito } from 'next/font/google'
 
-import Navbar from "./components/navbar/Navbar";
-import RegisterModal from "./components/modals/RegisterModal";
-import LoginModal from "./components/modals/LoginModal";
-import RentModal from "./components/modals/RentModal";
+import Navbar from '@/app/components/navbar/Navbar';
+import LoginModal from '@/app/components/modals/LoginModal';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import RentModal from '@/app/components/modals/RentModal';
 
-import getCurrentUser from "./actions/getCurrentUser";
-import ToasterProvider from "./providers/ToasterProvider";
+import ToasterProvider from '@/app/providers/ToasterProvider';
 
-const font = Nunito({ subsets: ["latin"] });
+import './globals.css'
+import ClientOnly from './components/ClientOnly';
+import getCurrentUser from './actions/getCurrentUser';
 
-export const metadata: Metadata = {
-  title: "Caregiver",
-  description: "Caregiver Next",
-};
+export const metadata = {
+  title: 'Caregiver',
+  description: 'Caregiver-Next',
+}
+
+const font = Nunito({ 
+  subsets: ['latin'], 
+});
 
 export default async function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   const currentUser = await getCurrentUser();
 
-  
   return (
-    <html lang="pt-br">
+    <html lang="en">
       <body className={font.className}>
-        <ToasterProvider />
-        <RentModal />
-        <LoginModal />
-        <RegisterModal />
-        <Navbar currentUser={currentUser} />
-        {children}
-        </body>
+        <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <RentModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
     </html>
-  );
+  )
 }
